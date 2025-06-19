7ec3c617cbce6be07cb01f33960900c7e074f713
Sven van de Scheur <svenvandescheur@gmail.com>
2025-06-19 14:26:05 +0200

    :bug: - fix: fix missing for delay lib

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./cache";
 export * from "./cookie";
 export * from "./debounce";
+export * from "./delay";
 export * from "./env";
 export * from "./format";
 export * from "./preference";
