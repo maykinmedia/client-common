8d2226dfbd04de8a006f2f98205a408c5a3a5b05
Sven van de Scheur <svenvandescheur@gmail.com>
2025-07-10 10:23:41 +0200

    :recycle: - refactor: merge debounce and delay lib in timing lib BREAKING CHANGE: debounce and delay lib no longer exist, use timing instead

--- src/timing/timing.ts
+++ src/timing/timing.ts
@@ -56,3 +56,12 @@ export function debounce<T extends (...args: Parameters<T>) => void>(
 
   return debouncedFn;
 }
+
+/**
+ * Returns a `Promise` which resolves after the specified timeout.
+ *
+ * @param timeout - Time in milliseconds to wait before resolving. Defaults to 300.
+ */
+export function delay(timeout = 300): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, timeout));
+}
